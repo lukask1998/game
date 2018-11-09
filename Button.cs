1656afca7cbadf5ef389b4f6a6ff031d73aa47f6
lukask1998 <44896233+lukask1998@users.noreply.github.com>
2018-11-09 13:08:38 +0200

    Create Button.cs

---
+++ Button.cs
@@ -0,0 +1,14 @@
+public class Button extends JButton {
+
+    private int index;
+
+    public Button(int index) {
+
+        super();
+        this.index = index;
+
+    }
+
+    public int getIndex() { return index; }
+
+}
